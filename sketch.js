b77356f1fc8ae7b8ed13d31bc39bb15230e2553d
zhihyen <413730333@o365.tku.edu.tw>
2025-05-23 11:08:53 +0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -95,5 +95,39 @@ function draw() {
         line(x1, y1, x2, y2);
       }
     }
+
+    // 新增左眼
+    const leftEye = [
+      243,190,56,28,27,29,30,247,130,25,110,24,23,22,26,112,
+      133,173,157,158,159,160,161,246,33,7,163,144,145,153,154,155
+    ];
+    stroke(255, 0, 255); // 粉紅色
+    strokeWeight(6);
+    for (let i = 0; i < leftEye.length - 1; i++) {
+      const idxA = leftEye[i];
+      const idxB = leftEye[i + 1];
+      if (keypoints[idxA] && keypoints[idxB]) {
+        const [x1, y1] = keypoints[idxA];
+        const [x2, y2] = keypoints[idxB];
+        line(x1, y1, x2, y2);
+      }
+    }
+
+    // 新增右眼
+    const rightEye = [
+      359,467,260,259,257,258,286,414,463,341,256,252,253,254,339,255,
+      263,466,388,387,386,385,384,398,362,382,381,380,374,373,390,249
+    ];
+    stroke(0, 255, 255); // 青色
+    strokeWeight(6);
+    for (let i = 0; i < rightEye.length - 1; i++) {
+      const idxA = rightEye[i];
+      const idxB = rightEye[i + 1];
+      if (keypoints[idxA] && keypoints[idxB]) {
+        const [x1, y1] = keypoints[idxA];
+        const [x2, y2] = keypoints[idxB];
+        line(x1, y1, x2, y2);
+      }
+    }
   }
 }
